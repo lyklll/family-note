ba95a747434711bbb0d02b32c7c2a6a40c94ff32
Jam <233376572@qq.com>
2019-06-12 18:16:39 +0800

    无可奉告

--- components/uni-icon-old/uni-icon.vue
+++ components/uni-icon-old/uni-icon.vue
@@ -37,8 +37,7 @@
 		font-family: uniicons;
 		font-weight: normal;
 		font-style: normal;
-		src: url('data:application/x-font-woff2;charset=utf-8;base64,d09GMgABAAAAAALQAAsAAAAABrwAAAKDAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHEIGVgCCcAqBSIE7ATYCJAMICwYABCAFhG0HMBvhBcg+QDy2kyBSRKU9lTZFa1PQf5+5PxewoNlDPDzt1+/cmVmb98WTCkSSilavJIiNkglJS9j9n7MM0Kg8v7GbAjpSI1/Ibzl9aY7U1DnDSp0jEO66GkAmZQNQuTGDiZy2xZrbgfgJOEDNGHCDwPHuN4aK6r80cQP0eS7Ht3yBXON7w2nNQYuPegHGAQW4F0aRFUjoDWM3ETzAxwSqWS2Fg4fPtlBXgFWBePnMs6BeKCiK0+4K7YaDRfzQ0k3f0vKdfz/+e9RJWhkYOPt44cL4d1A1f+2Dm/mrIkE/3kLGVqAQFxsbp5WEI5RUzbJFoCjl3/WnIXFUv3D+OtA9AKZ1wzMZselRuxMog1rbgWMmU0IDBdiG8OwPaTDfH0wDNxr1ItcVbRcz5sO2W2Egaqp5UBPN48basxrNRRxrXdgC2MP2lf34Wvr6pep7aT5NO3v79jaXtrz6Cmbz0BZ1oPPi5d3+FD8T3J0bfmx457GAXxZf0v9aJn9F94zJ4M8ZBw6V2AyCqYTGxlSy/fZWJlQVfWZmzN9Uz+RtxkK3YHSQdMwg61omCrMVLY1taOvag2qL1fHGOFESpYdNNxrCiCckQ34iG/FMFOYXLVP+0TYSJVSXY/zCxmoE9cB7spRctK8xcDg1KKxN0bsm7zy2JG/m4p4kD8rY7/TK7RWlJEtsyS+8gapBI5zgUnuO4pgxEw7J0Y6vmo26XdP0po7DCTQdIIhFEReyXUMBB0sZOl41K59fI55zMYt0SEbae0TkgulRX0dvALFypIMkj/JK7oJnQCkDGYIl0FKbR2IxhrLmeSHiUB3+hFpmpAtHmaGOzvau5O9OQAW8KeCsrP3uvcMuAQAA') format('woff2');
-  
+		src: url('//at.alicdn.com/t/font_1238376_ocysxihn0n.woff2') format('woff2');
 	}
 
 	.uni-icon {
@@ -58,6 +57,18 @@
 
 	.uni-icon-qrcode:before {
 		content: "\e76e";
+	}
+	
+	.uni-icon-send:before {
+		content: "\e916";
+	}
+	
+	.uni-icon-user:before {
+		content: "\e7ae";
+	}
+	
+	.uni-icon-team:before {
+		content: "\e7af";
 	}
 
 </style>
--- pages/tabbar/viewMine/viewMine.vue
+++ pages/tabbar/viewMine/viewMine.vue
@@ -3,10 +3,23 @@
 		<view class="head">
 			<image :src="memberInfo.avatarUrl" mode="scaleToFill" class="head-img"></image>
 			<view class="head-info">
-				<view class="head-pname">{{memberInfo.nickName}}</view>
+				<view class="head-pname">{{memberInfo.nickName}} <span>[爸爸]</span> </view>
 				<view class="head-fname">辛普森一家</view>
 			</view>
 			<icon class="er-code" type="qrcode" size="40" color="#888"></icon>
+		</view>
+		<view class="edit-tools">
+			<view class="tool-item">
+				<icon class="first-icon" type="team" size="20"></icon>
+				<view class="title">我的家庭</view>
+				<icon class="last-icon" type="send" size="16" ></icon>
+				
+			</view>
+			<view class="tool-item">
+				<icon class="first-icon" type="user" size="20"></icon>
+				<view class="title">个人资料</view>
+				<icon class="last-icon" type="send" size="16" ></icon>
+			</view>
 		</view>
 	</view>
 </template>
@@ -35,9 +48,11 @@
 
 <style lang="scss" scoped>
 	.viewMine {
-		padding-top: 69px;
+		background-color: #f6f6f6;
+		height: 100vh;
 		.head{
-			padding: 0 40upx 0 40upx;
+			background-color: #fff;
+			padding: 69px 40upx 40upx 40upx;
 			display: flex;
 			align-items: center;
 			.head-img{
@@ -55,6 +70,10 @@
 					font-size: $uni-font-size-lg;
 					font-weight: bold;
 					line-height: 60upx;
+					span{
+						font-weight:100;
+						color:#409EFF;
+					}
 				}
 				.head-fname{
 					font-size: $uni-font-size-sm;
@@ -64,6 +83,25 @@
 			}
 			.er-code{
 				
+			}
+		}
+		.edit-tools{
+			background-color: #fff;
+			margin-top: 20upx;
+			.tool-item{
+				padding: 20upx;
+				height: 60upx;
+				display: flex;
+				align-items: center;
+				border-bottom:1px solid #f6f6f6;
+				.first-icon{
+					
+				}
+				.title{
+					flex:1;
+					margin-left:15upx;
+				}
+				.last-icon{}
 			}
 		}
 	}
